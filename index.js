ae0958e97230291ed59efa62c62af067c8efc939
Ali El Mufti <alielmufti25@gmail.com>
2021-10-26 21:37:48 +0200

    multiple sources + new choosing source with url

--- index.js
+++ index.js
@@ -2,23 +2,71 @@ const PORT = 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const {response} = require("express");
 const articles = []
+const newspapers = [
+
+    {
+        name:'thetimes',
+        address: 'https://www.thetimes.co.uk/topic/bitcoin',
+        base: ''
+    },
+    {
+        name:'telegraph',
+        address: 'https://www.telegraph.co.uk/cryptocurrency/',
+        base: 'https://www.telegraph.co.uk'
+
+    },
+    {
+        name:'theguardian',
+        address: 'https://www.theguardian.com/technology/cryptocurrencies',
+        base: ''
+
+    },
+
+]
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address).then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        $('a:contains("crypto")', html).each(function () {
+            const title = $(this).text()
+            const url = $(this).attr('href')
+            articles.push({
+                title,
+                url:  newspaper.base+url,
+                source: newspaper.name
+            })
+        })
+    })
+})
 const app = express();
 app.get('/',(req,res)=>{
     res.json('Wecome to my Crypto News API')
 })
-app.get('/news',(req,res)=>{
-axios.get('https://www.theguardian.com/technology/cryptocurrencies').then((response)=>{
+app.get('/news',(req,res)=> {
+res.json(articles)
+})
+app.get('/news/:newspaperId', async(req,res)=> {
+    const newspaperId = req.params.newspaperId
+    const newsPaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newsPaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+axios.get(newsPaperAddress)
+    .then(response => {
     const html = response.data
     const $ = cheerio.load(html)
-    $('a:contains("crypto")', html).each(function(){
+    const specificArticles = []
+    $('a:contains("crypto")', html).each(function () {
         const title = $(this).text()
         const url = $(this).attr('href')
-        articles.push({
+        specificArticles.push({
             title,
-            url
+            url:  newsPaperBase+url,
+            source: newspaperId
         })
     })
-    res.json(articles)
-}).catch((err) => console.log(err))})
+        res.json(specificArticles)
+    }).catch(err => console.log(err))
+})
 app.listen(PORT, ()=> console.log(`server running on PORT ${PORT}`))
