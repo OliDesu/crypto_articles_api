705ea311fc095b4ff2e862c46a7102ca7a56acd0
Ali El Mufti <alielmufti25@gmail.com>
2021-10-27 00:47:13 +0200

    deployment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
@@ -7,22 +7,29 @@ const articles = []
 const newspapers = [
 
     {
-        name:'thetimes',
+        name: 'thetimes',
         address: 'https://www.thetimes.co.uk/topic/bitcoin',
         base: ''
     },
     {
-        name:'telegraph',
+        name: 'telegraph',
         address: 'https://www.telegraph.co.uk/cryptocurrency/',
         base: 'https://www.telegraph.co.uk'
 
     },
     {
-        name:'theguardian',
+        name: 'theguardian',
         address: 'https://www.theguardian.com/technology/cryptocurrencies',
         base: ''
 
     },
+    {
+        name: 'thenewyorktimes',
+        address: 'https://www.nytimes.com/topic/subject/bitcoin',
+        base: 'https://www.nytimes.com'
+
+    },
+
 
 ]
 newspapers.forEach(newspaper => {
@@ -34,39 +41,39 @@ newspapers.forEach(newspaper => {
             const url = $(this).attr('href')
             articles.push({
                 title,
-                url:  newspaper.base+url,
+                url: newspaper.base + url,
                 source: newspaper.name
             })
         })
     })
 })
 const app = express();
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.json('Wecome to my Crypto News API')
 })
-app.get('/news',(req,res)=> {
-res.json(articles)
+app.get('/news', (req, res) => {
+    res.json(articles)
 })
-app.get('/news/:newspaperId', async(req,res)=> {
+app.get('/news/:newspaperId', (req, res) => {
     const newspaperId = req.params.newspaperId
     const newsPaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
     const newsPaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
 
-axios.get(newsPaperAddress)
-    .then(response => {
-    const html = response.data
-    const $ = cheerio.load(html)
-    const specificArticles = []
-    $('a:contains("crypto")', html).each(function () {
-        const title = $(this).text()
-        const url = $(this).attr('href')
-        specificArticles.push({
-            title,
-            url:  newsPaperBase+url,
-            source: newspaperId
-        })
-    })
-        res.json(specificArticles)
-    }).catch(err => console.log(err))
+    axios.get(newsPaperAddress)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+            $('a:contains("crypto")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: newsPaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
 })
-app.listen(PORT, ()=> console.log(`server running on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
